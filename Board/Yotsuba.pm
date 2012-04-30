2e8ed31491ae04edeec18e7008e91919ceaec4d8
Eksopl <eksopl@gmail.com>
2012-04-30 23:34:09 +0100

    Fixing seconds support.

--- Board/Yotsuba.pm
+++ Board/Yotsuba.pm
@@ -58,7 +58,7 @@ sub parse_date($$){
 	my($text)=@_;
 	
 	my($mon,$mday,$year,$hour,$min,$sec)=
-		$text=~m!(\d+)/(\d+)/(\d+) \(\w+\) (\d+):(\d+)(?:(\d+))?!x;
+		$text=~m!(\d+)/(\d+)/(\d+) \(\w+\) (\d+):(\d+)(?::(\d+))?!x;
 	
 	use Time::Local;
 	timegm($sec or (time%60),$min,$hour,$mday,$mon-1,$year);
