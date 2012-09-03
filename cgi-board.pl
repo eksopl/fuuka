652066ed971de9fa97ea593242515cdb59e5011b
Eksopl <eksopl@gmail.com>
2012-09-03 01:18:30 +0100

    Ough.

--- cgi-board.pl
+++ cgi-board.pl
@@ -26,7 +26,7 @@ use Board::Mysql;
 use Board::Sphinx_Mysql;
 use Board::Yotsuba;
 
-BEGIN{-e "../board-config-local.pl" ? 
+BEGIN{-e "board-config-local.pl" ? 
 	require "board-config-local.pl" : require "board-config.pl"}
 
 sub html_encode($);
